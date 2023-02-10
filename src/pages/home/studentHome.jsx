9fe3045dbb7986274f602bd4fe8c76f993b5c104
Breno Wiebbelling <brenowdea@gmail.com>
2023-02-10 01:23:05 -0300

    starting table list

--- src/pages/home/studentHome.jsx
+++ src/pages/home/studentHome.jsx
@@ -1,9 +1,11 @@
 import { Profile } from '../../components/profile'
+import {DefaultTableList} from '../../components/tableList'
 
 export const StudentHome = () => {
   return (
     <div>
       <Profile />
+      <DefaultTableList/>
     </div>
   )
 }
